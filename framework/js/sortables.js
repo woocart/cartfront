46bab7b18c458c0c69103e57525b552798922c45
Akshit Sethi <akshitsethi@gmail.com>
2018-08-03 02:44:54 +0530

    Initial release of the homepage control addon.

---
+++ framework/js/sortables.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+	$('.cf-hc-components').sortable();
+	$('.cf-hc-components').disableSelection();
+
+	$('.cf-hc-components').bind('sortstop', function(e, ui) {
+		var components = new Array();
+		var disabled = '[disabled]';
+
+		$(e.target).find('li').each( function ( i, e ) {
+			if($(this).hasClass( 'disabled' ) ) {
+				components.push( disabled + $( this ).attr( 'id' ) );
+			} else {
+				components.push( $( this ).attr( 'id' ) );
+			}
+		});
+
+		components = components.join( ',' );
+
+		$( 'input[data-customize-setting-link="cf_hc_data"]' ).attr( 'value', components ).trigger( 'change' );
+	});
+
+	$( '.cf-hc-components .visibility' ).bind( 'click', function ( e ) {
+		var components = new Array();
+		var disabled = '[disabled]';
+
+		$( this ).parent( 'li' ).toggleClass( 'disabled' );
+
+		$( this ).parents( '.cf-hc-components' ).find( 'li' ).each( function ( i, e ) {
+			if ( $( this ).hasClass( 'disabled' ) ) {
+				components.push( disabled + $( this ).attr( 'id' ) );
+			} else {
+				components.push( $( this ).attr( 'id' ) );
+			}
+		});
+
+		components = components.join( ',' );
+
+		$( 'input[data-customize-setting-link="cf_hc_data"]' ).attr( 'value', components ).trigger( 'change' );
+	});
+});
