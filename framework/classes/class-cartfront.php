861e97f1abed8dc15b7243992ec879bdad7792f4
Akshit Sethi <akshitsethi@gmail.com>
2018-08-20 19:57:40 +0530

    Prepared base for unit tests.

--- framework/classes/class-cartfront.php
+++ framework/classes/class-cartfront.php
@@ -6,22 +6,22 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 if ( ! class_exists( 'Cartfront' ) ) :
-class Cartfront {
+	class Cartfront {
 
-    /**
-     * Constructor function.
-     *
-     * @access  public
-     * @since   1.0.0
-     * @return  void
-     */
-    public function __construct() {
-        
-    }
+		/**
+		 * Constructor function.
+		 *
+		 * @access  public
+		 * @since   1.0.0
+		 * @return  void
+		 */
+		public function __construct() {
 
-}
+		}
+
+	}
 endif;
---
+++ tests/TestTheme.php
@@ -0,0 +1,17 @@
+<?php
+
+class TestTheme extends \PHPUnit\Framework\TestCase {
+
+	function setUp() {
+		\WP_Mock::setUsePatchwork( true );
+		\WP_Mock::setUp();
+	}
+
+	function tearDown() {
+		$this->addToAssertionCount(
+			\Mockery::getContainer()->mockery_getExpectationCount()
+		);
+		\WP_Mock::tearDown();
+	}
+
+}
