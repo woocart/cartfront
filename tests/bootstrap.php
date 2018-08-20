4fa508b33f3d7f09d6f7aa88dfcc43a0b89cb42e
Akshit Sethi <akshitsethi@gmail.com>
2018-08-21 01:11:32 +0530

    Basic tests for footer bar addon.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,4 @@ define( 'ABSPATH', true );
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/antecedent/patchwork/Patchwork.php';
 require_once __DIR__ . '/../framework/classes/class-cartfront.php';
+require_once __DIR__ . '/../framework/classes/class-cartfront_footer_bar.php';
