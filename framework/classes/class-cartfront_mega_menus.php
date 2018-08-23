1cf0ad8ab3a0188d24bb5748a7d1647bf48f7812
Akshit Sethi <akshitsethi@gmail.com>
2018-08-24 04:55:34 +0530

    WIP

--- framework/classes/class-cartfront_mega_menus.php
+++ framework/classes/class-cartfront_mega_menus.php
@@ -17,10 +17,50 @@ class Cartfront_Mega_Menus {
      *
      * @access  public
      * @since   1.0.0
-     * @return  void
      */
     public function __construct() {
-        
+        add_action( 'admin_init', array( &$this, 'check_settings' ) );
+        add_action( 'widgets_init', array( &$this, 'register_widget' ) );
+        add_action( 'after_setup_theme', array( &$this, 'register_nav_menus' ) );
+        add_filter( 'wp_nav_menu_args', array( &$this, 'modify_nav_menu_args' ), PHP_INT_MAX );
+        add_filter( 'wp_nav_menu', array( &$this, 'add_responsive_toggle' ), 10, 2 );
+    }
+
+    /**
+     * Check for the menu settings and if not present, add defaults.
+     *
+     * @access public
+     */
+    public function check_settings() {
+        $settings = get_option( 'cf_menu_settings' );
+
+        if ( ! $settings ) {
+            $settings['prefix']         = 'disabled';
+            $settings['descriptions']   = 'enabled';
+
+            add_option( 'cf_menu_settings', $settings );
+        }
+    }
+
+    /**
+     * Array of classes 
+     *
+     * @return array
+     * @codeCoverageIgnore
+     */
+    private function classes() {
+        $classes = array(
+            'cf_menu_walker'            => 'class-cartfront_menu_walker.php',
+            'cf_menu_widget_manager'    => 'class-cartfront_menu_widget_manager.php',
+            'cf_menu_item_manager'      => 'class-cartfront_menu_item_manager.php',
+            'cf_menu_nav'               => 'class-cartfront_menu_nav.php',
+            'cf_menu_style_manager'     => 'class-cartfront_menu_style_manager.php',
+            'cf_menu_settings'          => 'class-cartfront_menu_settings.php',
+            'cf_menu_widget'            => 'class-cartfront_menu_widget.php',
+            'cf_menu_blocks'            => 'class-cartfront_menu_blocks.php'
+        );
+
+        return $classes;
     }
 
 }
