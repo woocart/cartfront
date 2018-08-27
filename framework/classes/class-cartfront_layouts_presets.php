5c769d45a3635fbfa9da5d7492af5959d8843227
Akshit Sethi <akshitsethi@gmail.com>
2018-08-27 13:22:43 +0530

    Remove temporary changes made for the  store.

--- framework/classes/class-cartfront_layouts_presets.php
+++ framework/classes/class-cartfront_layouts_presets.php
@@ -39,22 +39,44 @@ class Cartfront_Layouts_Presets {
         ) );
 
         /**
-         * Presets.
+         * Layout.
          */
-        $wp_customize->add_setting( 'cf_lp_presets', array(
+        $wp_customize->add_setting( 'cf_lp_layout', array(
             'default'           => 'default',
             'sanitize_callback' => 'storefront_sanitize_choices'
         ) );
 
-        $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'cf_lp_presets', array(
-            'label'    => esc_attr__( 'Post meta display', 'cartfront' ),
-            'section'  => 'cf_lp_presets',
-            'settings' => 'cf_lp_presets',
-            'type'     => 'select',
-            'priority' => 10,
-            'choices'  => array(
+        $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'cf_lp_layout', array(
+            'label'         => esc_html__( 'Select Layout', 'cartfront' ),
+            'description'   => esc_html__( 'From the below options, select the one that fits best for your store.', 'cartfront' ),
+            'section'       => 'cf_lp_presets',
+            'settings'      => 'cf_lp_layout',
+            'type'          => 'select',
+            'priority'      => 10,
+            'choices'       => array(
                 'default'   => esc_html__( 'Default Layout', 'cartfront' ),
                 'toys'      => esc_html__( 'Toys Store', 'cartfront' )
+            )
+        ) ) );
+
+        /**
+         * Color scheme.
+         */
+        $wp_customize->add_setting( 'cf_lp_color_scheme', array(
+            'default'           => 'default',
+            'sanitize_callback' => 'storefront_sanitize_choices'
+        ) );
+
+        $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'cf_lp_color_scheme', array(
+            'label'         => esc_html__( 'Select Color Scheme', 'cartfront' ),
+            'description'   => esc_html__( 'Changing color scheme will override the settings you have set manually.', 'cartfront' ),
+            'section'       => 'cf_lp_presets',
+            'settings'      => 'cf_lp_color_scheme',
+            'type'          => 'select',
+            'priority'      => 15,
+            'choices'       => array(
+                'default'   => esc_html__( 'Default Color Scheme', 'cartfront' ),
+                'toys'      => esc_html__( 'Toys Store Color Scheme', 'cartfront' )
             )
         ) ) );
     }
@@ -62,29 +84,8 @@ class Cartfront_Layouts_Presets {
     /**
      * Modifications for the presets.
      */
-
-    /**
-         * Functions hooked into storefront_header action
-         *
-         * @hooked storefront_header_container                 - 0
-         * @hooked storefront_skip_links                       - 5
-         * @hooked storefront_social_icons                     - 10
-         * @hooked storefront_site_branding                    - 20
-         * @hooked storefront_secondary_navigation             - 30
-         * @hooked storefront_product_search                   - 40
-         * @hooked storefront_header_container_close           - 41
-         * @hooked storefront_primary_navigation_wrapper       - 42
-         * @hooked storefront_primary_navigation               - 50
-         * @hooked storefront_header_cart                      - 60
-         * @hooked storefront_primary_navigation_wrapper_close - 68
-         */
     public function presets_header() {
-        remove_action( 'storefront_header', 'storefront_secondary_navigation', 30 );
-        remove_action( 'storefront_header', 'storefront_product_search', 40 );
-        remove_action( 'storefront_header', 'storefront_header_cart', 60 );
-
-        add_action( 'storefront_header', 'storefront_product_search', 30 );
-        add_action( 'storefront_header', 'storefront_header_cart', 40 );
+        
     }
 
 }
